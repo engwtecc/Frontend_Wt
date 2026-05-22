262434824e2dc466bb21d39d890a64d68d7afee0
engwtecc <engenharia@wtecc.com.br>
2026-05-22 10:16:09 -0300

    Update AdminVerRelatorio.tsx

--- src/pages/AdminVerRelatorio.tsx
+++ src/pages/AdminVerRelatorio.tsx
@@ -116,6 +116,9 @@ export default function AdminVerRelatorio() {
   return (
     <>
       <Paper sx={{ p: 3, mb: 3 }}>
+        <Typography variant="h4" fontWeight="bold">
+          {relatorio.colaborador_nome}
+        </Typography>
         <Typography variant="h5" gutterBottom>
           Relatório do Dia {relatorio.data}
         </Typography>
